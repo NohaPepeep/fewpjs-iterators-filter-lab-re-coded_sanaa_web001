76929fbcc46b0d0534bef78472a3376db9407181
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-16 15:32:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,13 @@ function findMatching(array,str){
     })
     return newArr;
     }
+  function findMatching(array,str){
+  const newArr=[];
+    array.filter(function(item){
+        if(item.toLowerCase()===str.toLowerCase()){
+          newArr.push(item);
+        }
+    })
+    return newArr;
+    }  
+    
